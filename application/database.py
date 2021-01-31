c611cabe474ce01cade1122e556367a624d85d4c
filon13p <filon13p@gmail.com>
2021-01-31 13:50:38 +0300

    *req psycorg2

--- application/database.py
+++ application/database.py
@@ -1,7 +1,7 @@
-from psycopg2 import *
+import psycopg2
 from config import Config
+
 from urllib.parse import urlparse
-from contextlib import closing
 
 db = None
 
@@ -9,15 +9,20 @@ db = None
 def db_open():
     global db
     if db is None:
-        url = urlparse.urlparse(Config.DATABASE_URI)
+        url = Config.DATABASE_URI
+        """
+        
         dbname = url.path[1:]
         user = url.username
         password = url.password
         host = url.hostname
         port = url.port
-
-        db = connect(dbname=dbname, user= user,
-                     password=password , host=host, port=port)
-    # db = connect(dbname=Config.DB_NAME, user=Config.DB_USER,
-    #            password=Config.DB_PASS, host=Config.DATABASE_URI, port=Config.DB_PORT)
+        """
+        db = psycopg2.connect(url)
+        """
+        #db = connect(dbname=dbname, user= user,
+        #            password=password , host=host, port=port)
+        # db = connect(dbname=Config.DB_NAME, user=Config.DB_USER,
+        #            password=Config.DB_PASS, host=Config.DATABASE_URI, port=Config.DB_PORT)
+        """
     return db
