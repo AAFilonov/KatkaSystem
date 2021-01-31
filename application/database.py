96f0590b1493920d31ba92b57f1c62cd5251bc76
filon13p <filon13p@gmail.com>
2021-01-31 13:36:11 +0300

    *heroku db connect redone

--- application/database.py
+++ application/database.py
@@ -1,5 +1,6 @@
 from psycopg2 import *
 from config import Config
+from urllib.parse import urlparse
 from contextlib import closing
 
 db = None
@@ -8,6 +9,15 @@ db = None
 def db_open():
     global db
     if db is None:
-        db = connect(dbname=Config.DB_NAME, user=Config.DB_USER,
-                     password=Config.DB_PASS, host=Config.DATABASE_URI, port=Config.DB_PORT)
+        url = urlparse.urlparse(Config.DATABASE_URI)
+        dbname = url.path[1:]
+        user = url.username
+        password = url.password
+        host = url.hostname
+        port = url.port
+
+        db = connect(dbname=dbname, user= user,
+                     password=password , host=host, port=port)
+    # db = connect(dbname=Config.DB_NAME, user=Config.DB_USER,
+    #            password=Config.DB_PASS, host=Config.DATABASE_URI, port=Config.DB_PORT)
     return db
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'key'
-    DATABASE_URI = os.environ.get('DATABASE_URL') or 'localhost'
+    DATABASE_URI = os.environ.get('DATABASE_URL')
     DB_PORT = 5432
     DB_USER = 'postgres'
     DB_PASS = 'pass'
