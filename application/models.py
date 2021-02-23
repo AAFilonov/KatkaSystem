677385c409c71500c99736f64c73ba6da7aae0ff
filon13p <filon13p@gmail.com>
2021-02-23 16:15:20 +0300

    *миграции

--- application/models.py
+++ application/models.py
@@ -54,6 +54,7 @@ class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     body = db.Column(db.String(140))
+    bio = db.Column(db.String(200))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
