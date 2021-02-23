74c8c688818e4d6298ebe29216d14adf052c0dcb
filon13p <filon13p@gmail.com>
2021-02-23 15:19:40 +0300

    +Сделал систему аутентификации

--- application/models.py
+++ application/models.py
@@ -1,12 +1,22 @@
 from application import db, login
 import werkzeug.security as security
 import flask_login
+from typing import Final
 
 
 class User(flask_login.UserMixin, db.Model):
+    # статические константы для опредления классов
+    ROLE_USER: Final = 1
+    ROLE_ADMIN: Final = 2
+
+    # поля класса
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    role = db.Column(db.Integer)
+
+    def is_admin(self):
+        return self.role == User.ROLE_ADMIN
 
     def set_password(self, password):
         self.password_hash = security.generate_password_hash(password)
@@ -14,12 +24,31 @@ class User(flask_login.UserMixin, db.Model):
     def check_password(self, password):
         return security.check_password_hash(self.password_hash, password)
 
-    @login.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@login.request_loader
+def load_user(request):
+    token = request.headers.get('Authorization')
+    if token is None:
+        token = request.args.get('token')
+
+    if token is not None:
+        username, password = token.split(":")  # naive token
+        user_entry = User.get(username)
+        if (user_entry is not None):
+            user = User(user_entry[0], user_entry[1])
+            if (user.password == password):
+                return user
+    return None
 
 
 class Character(db.Model):
--- application/routes.py
+++ application/routes.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 from flask import *
 
-from flask_login import current_user, login_user ,logout_user
+from flask_login import current_user, login_user ,logout_user, login_required
 from application.models import User
 import application.forms as Forms
-
 from application import app, db
 
-
+from flask import request
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'test'}
-    return render_template('index.html', user=user, title='Home Page')
+
+    return render_template('index.html', user=current_user, title='Home Page')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -27,11 +28,36 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = Forms.RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, role=User.ROLE_USER)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/admin')
+@login_required
+def admin():
+    return redirect(url_for('admin'))
+
+
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
